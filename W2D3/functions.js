77d6cf5dc0f96ad93c2d36c65e8b27c04eb150fb
BIT student <classroom@bgit.rs>
2018-10-10 18:22:00 +0200

    funkcije

--- W2D3/functions.js
+++ W2D3/functions.js
@@ -26,3 +26,156 @@
 // } else {
 //     console.log("The string isn't palindrom!");
 // }
+
+
+// function sum(a, b) {
+//     var result;
+//     result = a + b;
+//     console.log(arguments);
+//     return result;
+// }
+
+// var final_result;
+// final_result = sum(11, 90);
+
+// console.log(final_result);
+
+// function sum_of_all_arguments() {
+//     var result = 0;
+//     var i;
+//     for (i = 0; i < arguments.length; i++) {
+//         result += arguments[i];
+//     }
+
+//     return result;
+
+// }
+
+// var s;
+// s = sum_of_all_arguments(3, 6, 7, 4, 5);
+// console.log(s);
+
+// var a = 44;
+// var b = 7;
+// function max(a, b) {
+//     var maxnum;
+//     if (a > b) {
+//         maxnum = a
+//     }
+//     else {
+//         maxnum = b
+//     }
+//     return maxnum;
+// }
+// var maxnum = max(a, b);
+// console.log(maxnum)
+
+
+
+
+// var a = 45;
+
+// function oddchek(a) {
+//     if (a % 2 == 1) {
+//         console.log("The number is odd");
+//     }
+//     else {
+//         console.log("The number is even");
+//     }
+
+// }
+// oddchek(a);
+
+
+
+// var num = 552245;
+
+// function digit(a) {
+//     var str = ""
+//     str = str + a
+//     if (str.length == 3) {
+//         console.log("number is three digit")
+//     }
+//     else {
+//         console.log("number is not three digit")
+//     }
+// }
+
+// digit(num);
+
+// function arithmeticMean() {
+//     var result = 0;
+//     var i;
+
+//     for (i = 0; i < arguments.length; i++) {
+//         result += arguments[i];
+//     }
+
+//     return result / arguments.length;
+
+
+// }
+
+// console.log(arithmeticMean(3, 4, 3, 4));
+
+// var a = 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111;
+
+// function numdig(h) {
+//     var str = "";
+//     str = str + h;
+//     console.log(str);
+//     console.log(str.length);
+// }
+// numdig(a);
+
+// var a = 1445;
+
+// function dignum(a) {
+//     var conter = 0;
+//     for (var i = a; i > 1; i = i / 10) {
+//         conter = conter + 1;
+//     }
+//     return conter;
+// }
+// console.log(dignum(a));
+
+
+// var a = "saas";
+
+// function isString(a) {
+//     if (typeof a == "string") {
+//         console.log("true")
+//     } else {
+//         console.log("false")
+//     }
+
+// }
+
+// isString(a);
+
+
+// var a = "Hello";
+// function prazna(a) {
+//     if (a == "hello") {
+//         console.log("true");
+//     }
+//     else {
+//         console.log("false");
+//     }
+
+// }
+// prazna(a);
+
+function concatinator(string, n) {
+    var result = "";
+    if (typeof string == "string" && typeof n == "number") {
+        for (var i = 0; i < n; i++) {
+            result += string;
+        }
+    } else {
+        console.log("False entery! ex('string', number)");
+    }
+    return result;
+}
+
+console.log(concatinator("hahahahahah", 5));
