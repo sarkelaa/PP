1f2c938e0c117ca327d8e5d5670880f5cbf74e1f
BIT student <classroom@bgit.rs>
2018-10-10 15:16:52 +0200

    .

---
+++ W2D3/functions.js
@@ -0,0 +1,28 @@
+// var s = "anavolimilovana";
+// var i;
+// var ind = true;
+
+// for (i = 0; i < s.length && ind; i++) {
+//     if (s[i] != s[(s.length - 1) - i] || i == s.length / 2) {
+//         ind = false;
+//         console.log("The string isn't palindrom!");
+//     }
+// }
+
+// if (ind == true) {
+//     console.log("The string is palindrom!");
+// }
+
+// var p = "anavolimilovana";
+// var i;
+// var q = "";
+
+// for (i = p.length - 1; i >= 0; i--) {
+//     q += p[i];
+// }
+
+// if (p == q) {
+//     console.log("The string is palindrom!");
+// } else {
+//     console.log("The string isn't palindrom!");
+// }
